ef6a656304d3944e792aa5003226aeeb7f138db2
elnatty <alabinathan1916@gmail.com>
2022-05-29 22:00:59 +0100

    using break with while loop

---
+++ 21_Keywords_in_.c
@@ -0,0 +1,39 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+  /* 
+    1: 'goto' keyword: if the "if" statement is true, then it executes the invalidator statement
+  */
+
+  // int i = 50;
+  //   repeat_from_here:		// the label ( destination )
+  //   	printf("%d\n",i++);
+  //   if( i <= 10 )				// Until the condition is satisified
+  //   	goto repeat_from_here;	// the control will jump to the label ( source )
+	// return 0;
+
+  // int age=40;
+  // if (age <= 0)
+  //   {
+  //     goto invalid_entry;
+  //   }
+  // invalid_entry:
+  //   printf("invalid entry...");
+
+
+  /*
+    2: "break" and "continue" keywords.
+        >> continue: is used to skip an execution if a certain condition is met.
+  */
+  // int i;
+  // while ((i<=10)){
+  //   printf("%d\n", i);
+  //   if (i == 5){
+  //     break;
+  //   }
+  //   i++;
+  // }
+  // printf("This loop has ended....\n");
+
+}
