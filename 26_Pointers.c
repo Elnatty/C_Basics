c12d2424c112708ad9cc362c3ee042a0b7a932ec
elnatty <alabinathan1916@gmail.com>
2022-05-31 19:33:02 +0100

    pointers in c

---
+++ 26_Pointers.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+  // pointers in C: are variables which stores address location of another variable. and it can be used to derive values stored at that address location.
+  int age=23;
+  *(&age);    // meaning value at address of age.
+  &age;        // gives address location of age.
+
+  // using pointers
+  int a = 10;
+  int *val = &a;
+  printf("pointer of a : %d\n", *(&a)); //or
+  printf("pointer of a : %d\n", *&a); //or
+  printf("pointer of a : %d\n", *val);
+
+}
