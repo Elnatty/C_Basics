76a28d39bd09d5f0fc5766e25305821f61ae6039
elnatty <alabinathan1916@gmail.com>
2022-05-31 18:27:56 +0100

    programs on multi arrays

---
+++ 25_Multidimentional_Arrays.c
@@ -0,0 +1,55 @@
+#include<conio.h>
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+  // multi-array 1:
+  // int ar_1[2][3] = {{10, 20, 30},
+  //                   {100,200,300}};
+  // // accessing..
+  // // printf("%d", ar_1[0][2]);   // outputs 30.
+
+
+
+  // // multi-array char..
+  // char ar_2[][4] = {{'A', 'E', 'I', 'O', 'U'},
+  //                   {'a', 'e', 'i', 'o', 'u'}};
+  // // accessing..
+  // printf("%c", ar_2[1][2]);   // outputs i.
+
+
+  // array 3:
+  // int arr_3[4][5] = {{12,34,13,54,65},
+  //                    {57,23,66,32,67},
+  //                    {19,18,35,82,71},
+  //                    {31,73,29,70,90}};
+
+  // printf("%d", arr_3[3][2]);    // outputs 29.
+
+
+
+  // programs on multi-dimentional arrays.
+  // int i, j, m, n, a[5][5];
+  // printf("\nenter order of matrix: ");
+  // scanf("%d%d", &m, &n);
+
+  // printf("\nyou entered %d by %d matrix\n", m, n);
+
+  // for (i=0; i<m; i++){
+  //   // collecting user inputs.
+  //   for (j=0; j<n; j++){
+  //     printf("enter figures %dx%d >> ", i,j);
+  //     scanf("%d", &a[i][j]);
+  //   }
+  // }
+  //   for (i=0; i<m; i++){
+  //     for (j=0; j<n; j++){
+  //       printf("\n%dx%d=%d", i, j, a[i][j]);
+  //     }
+  //     printf("\n");
+  //   }
+
+
+  // 
+
+}
