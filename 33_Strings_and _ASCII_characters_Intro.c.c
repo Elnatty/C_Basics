313349f6f952ec87c4a017c3db35d557c2e33656
elnatty <alabinathan1916@gmail.com>
2022-06-01 19:16:42 +0100

    intro to strings and ascii characters

---
+++ 33_Strings_and _ASCII_characters_Intro.c.c
@@ -0,0 +1,32 @@
+#include<conio.h>
+#include<string.h>
+#include<stdio.h>
+
+int main(){
+  // char my_str[6]="dking";
+  // printf("my name is %s.\n", my_str);
+
+  /* 
+  Built-in Functions for strings:
+  1: strcpy(s1, s2);    ---> copies s2 into s1.
+  2: strcat(s1, s2);    ---> concatenate s2 into the end of s1.
+  3: strlen(s1);        ---> returns length of s1.
+  4: strcmp(s1, s2);    ---> compares if s1 and s2 are the same;
+                              returns 0         -> is s1 and s2 are same.
+                              less than 0       -> if s1 < s2.
+                              greater than 0    -> if s1 > s2.
+  
+  5: strlwr(s1);        ---> converts s1 to lower case.
+  6: strupr(s1);        ---> converts s1 to upper case.
+    e.t.c....
+  */
+
+ /*
+    ASCII Characters:
+      A - Z is 65 - 91.
+      a - z is 97 - 122.
+      0 - 9 is 48 - 57.
+      NOTE:
+        There are total of 256 ascii characters.. ie from 0 - 255.
+ */
+}
