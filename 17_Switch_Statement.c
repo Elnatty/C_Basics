28ae547131dd3c052780736bcacd4f5e6a4bf7d2
elnatty <alabinathan1916@gmail.com>
2022-06-04 11:32:58 +0100

    checking grades of a student

--- 17_Switch_Statement.c
+++ 17_Switch_Statement.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 #include <conio.h>
-int main()
+ int main()
 {
-  // create a program where if the value of x is 1 -> outputs "one", 2 -> outputs "Two", etc.... 
-  int x=33;
-  switch (x)
-    {
-      case 1:
-        printf("One\n");
-        break;
-      case 2:
-        printf("Two\n");
-        break;
-      case 3:
-        printf("Three");
-        break;
-      default:
-        printf("Not a valid number..");
-    }
+//   // create a program where if the value of x is 1 -> outputs "one", 2 -> outputs "Two", etc.... 
+//   int x=33;
+//   switch (x)
+//     {
+//       case 1:
+//         printf("One\n");
+//         break;
+//       case 2:
+//         printf("Two\n");
+//         break;
+//       case 3:
+//         printf("Three");
+//         break;
+//       default:
+//         printf("Not a valid number..");
+//     }
 
+
+
+  char grade='F';
+  switch (grade){
+    case 'A':
+      printf("You did Great.");
+      break;
+    case 'B':
+      printf("You did Alright.");
+      break;
+    case 'C':
+      printf("You did poorly.");
+      break;
+    case 'D':
+      printf("You did very poorly.");
+      break;
+    case 'F':
+      printf("You did very badly.");
+      break;
+    default :
+      printf("invalid grade..");
+  }
 }
