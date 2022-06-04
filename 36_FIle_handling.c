f831a76bb227a060cc1409aae3665bbfe9dfc239
elnatty <alabinathan1916@gmail.com>
2022-06-04 21:39:32 +0100

    notes on file handling

--- 36_FIle_handling.c
+++ 36_FIle_handling.c
@@ -1,5 +1,24 @@
 #include<conio.h>
 #include<stdio.h>
+/*
+NOTES: 
+"r" --> reading from a file.
+"w" --> writing to a file, contents are overwritten.
+"a" --> adds new content to end of the file.
+"r+" --> read, write and modify existing content.
+"w+" --> contents are overwritten, write new content, modify existing, reading back ne content.
+"a+" --> read existing, appending new content, modify existing content.
+
+----->> "a", "w+", "a+" creates a new file if specified file does not exist.
+
+fopen --> open file in specified mode.
+fclose --> close a file.
+getc --> read character current cursor location.
+putc --> writes charater on current cursor location.
+fgets --> reads a line from file.
+fpts --> writes a line to file.
+EOF --> end of file..
+*/
 int main()
 {
   // initialising a new file
